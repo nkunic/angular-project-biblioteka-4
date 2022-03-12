20fd5018bb21f58f5094a49fe1513ce6d0a77e61
nkunic <natasa.kunic@gmail.com>
2022-03-12 16:45:00 +0100

    funkcije

--- src/app/views/knjige/knjiga/knjiga.component.ts
+++ src/app/views/knjige/knjiga/knjiga.component.ts
@@ -1,12 +1,27 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { BibliotekaService } from '../../../services/biblioteka.service';
 
 @Component({
   selector: 'app-knjiga',
   templateUrl: './knjiga.component.html',
   styleUrls: ['./knjiga.component.scss'],
 })
-export class KnjigaComponent {
+export class KnjigaComponent implements OnInit {
   @Input() id: number = 0;
   @Input() autor: string = '';
   @Input() naslov: string = '';
+
+  public detaljnije = false;
+
+  constructor(private bibliotekaService: BibliotekaService) {}
+
+  ngOnInit() {}
+
+  public prikaziDetaljnije() {
+    this.detaljnije = !this.detaljnije;
+  }
+
+  public sacuvaj() {
+    this.bibliotekaService.knjige[this.id].naslov = this.naslov;
+  }
 }
