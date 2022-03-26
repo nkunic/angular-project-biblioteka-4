7b6994a4a384993bfc26f651a9156515ddd29e97
nkunic <natasa.kunic@gmail.com>
2022-03-26 14:57:44 +0100

    1 file modified

--- src/app/views/clanovi/clan/clan.component.ts
+++ src/app/views/clanovi/clan/clan.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Knjiga } from '../../../model/knjiga.model';
 
 @Component({
   selector: 'app-clan',
@@ -8,5 +9,5 @@ import { Component, Input } from '@angular/core';
 export class ClanComponent {
   @Input() id: number = 0;
   @Input() ime: string = '';
-  @Input() knjiga: any;
+  @Input() knjiga: Knjiga;
 }
